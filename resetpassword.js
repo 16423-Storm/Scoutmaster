8d0015ee971349024e43bbcaa38773ced47f80be
abcde456 <yellieth2575@hwdsb.on.ca>
2025-08-21 20:59:14 -0400

    changing supabase to supabaseclientish

--- resetpassword.js
+++ resetpassword.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://tgptsuzheleshmtesbcx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncHRzdXpoZWxlc2htdGVzYmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4ODA3NDQsImV4cCI6MjA3MDQ1Njc0NH0.pNTxZbSUeyATBlssBIZDrTyn1E2fr8bvCQ4mP3OQ-JM';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClientish = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function resetPassword() {
 	const statusText = document.getElementById("statustext");
@@ -14,7 +14,7 @@ async function resetPassword() {
 	}
 
 	try {
-		const { data, error } = await supabase.auth.updateUser({
+		const { data, error } = await supabaseClientish.auth.updateUser({
 			password: newPassword
 		});
 
