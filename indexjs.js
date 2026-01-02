f45a6b35023f7a19f0d82adcbe4257651bb6140e
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-02 11:41:06 -0500

    Fixing double """" issue

--- indexjs.js
+++ indexjs.js
@@ -2130,10 +2130,10 @@ function setPrescoutCustomQuestions(questionData, finalized){
 
                 var newDiv2 = document.createElement("div");
                 newDiv2.classList.add("abilitycontainer");
-                newDiv2.innerHTML = `<input class="prescoutsmallinput" id="ability${index + 1}"><button class="removecustomquestionbutton" onclick="popUpWarning("This action will reset ALL scouting data currently gathered, are you sure you want to continue?", () => removeCustomQuestion(${index + 1})">-</button>`;
+                newDiv2.innerHTML = `<input class="prescoutsmallinput" id="ability${index + 1}"><button class="removecustomquestionbutton" onclick="popUpWarning('This action will reset ALL scouting data currently gathered, are you sure you want to continue?', () => removeCustomQuestion(${index + 1}))">-</button>`;
                 document.getElementById("addQuestionMarkerDiv").before(newDiv2);
             } else {
-                newDiv.innerHTML = `<p class="prescoutsmalltext">${question}:&#8203;</p><input class="prescoutcheckbox" type="checkbox" id="ability${index + 1}"><button class="removecustomquestionbutton" onclick="popUpWarning("This action will reset ALL scouting data currently gathered, are you sure you want to continue?", () => removeCustomQuestion(${index + 1})">-</button>`;
+                newDiv.innerHTML = `<p class="prescoutsmalltext">${question}:&#8203;</p><input class="prescoutcheckbox" type="checkbox" id="ability${index + 1}"><button class="removecustomquestionbutton" onclick="popUpWarning('This action will reset ALL scouting data currently gathered, are you sure you want to continue?', () => removeCustomQuestion(${index + 1}))">-</button>`;
                 document.getElementById("addQuestionMarkerDiv").before(newDiv);
             }
         });
@@ -2151,14 +2151,14 @@ function setPrescoutCustomQuestions(questionData, finalized){
 
                 const newDiv2 = document.createElement("div");
                 newDiv2.classList.add("abilitycontainer");
-                newDiv2.innerHTML = `<input class="prescoutsmallinput" id="ability${String(abilityNumber)}" value="${questionData.abilities[String(abilityNumber)] ?? ""}" disabled><button class="removecustomquestionbutton" onclick="popUpWarning("This action will reset ALL scouting data currently gathered, are you sure you want to continue?", () => removeCustomQuestion(${abilityNumber})">-</button>`;
+                newDiv2.innerHTML = `<input class="prescoutsmallinput" id="ability${String(abilityNumber)}" value="${questionData.abilities[String(abilityNumber)] ?? ""}" disabled><button class="removecustomquestionbutton" onclick="popUpWarning('This action will reset ALL scouting data currently gathered, are you sure you want to continue?', () => removeCustomQuestion(${abilityNumber}))">-</button>`;
                 document.getElementById("addQuestionMarkerDiv").before(newDiv2);
             } else {
                 // Checkbox version
                 const newDiv = document.createElement("div");
                 newDiv.classList.add("abilitycontainer");
                 const isChecked = questionData.abilities[String(abilityNumber)] ? "checked" : "";
-                newDiv.innerHTML = `<p class="prescoutsmalltext">${question}:&#8203;</p><input class="prescoutcheckbox" type="checkbox" id="ability${String(abilityNumber)}" ${isChecked} disabled><button class="removecustomquestionbutton" onclick="popUpWarning("This action will reset ALL scouting data currently gathered, are you sure you want to continue?", () => removeCustomQuestion(${abilityNumber})">-</button>`;
+                newDiv.innerHTML = `<p class="prescoutsmalltext">${question}:&#8203;</p><input class="prescoutcheckbox" type="checkbox" id="ability${abilityNumber}" ${isChecked} disabled><button class="removecustomquestionbutton" onclick="popUpWarning('This action will reset ALL scouting data currently gathered, are you sure you want to continue?', () => removeCustomQuestion(${abilityNumber}))">-</button>`;
                 document.getElementById("addQuestionMarkerDiv").before(newDiv);
             }
         });
