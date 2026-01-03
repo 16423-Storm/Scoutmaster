122899c1bee82f624e9ba23b00254acc52947e7d
abcde456 <yellieth2575@hwdsb.on.ca>
2026-01-02 19:11:15 -0500

    Added fix to supabase bug with question removal

--- indexjs.js
+++ indexjs.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://tgptsuzheleshmtesbcx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncHRzdXpoZWxlc2htdGVzYmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4ODA3NDQsImV4cCI6MjA3MDQ1Njc0NH0.pNTxZbSUeyATBlssBIZDrTyn1E2fr8bvCQ4mP3OQ-JM';
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, { telemetry: false });
 
 const savedGroupId = localStorage.getItem('groupId');
 const savedSupabaseUrl = localStorage.getItem('supabaseUrl');
